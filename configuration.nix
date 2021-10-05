e55b8a3083e538477b0ce5e849cd2326d9bfde2e
System administrator <2446682260@qq.com>
2021-10-05 09:55:40 +0800

    modify config

--- configuration.nix
+++ configuration.nix
@@ -9,55 +9,9 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
 #      ./etc/git.nix
-      ./etc/zsh.nix
-      ./sys/auto.nix
-      ./sys/boot.nix
-      ./sys/default.nix
-      ./sys/desktop.nix
-      ./sys/fonts.nix
-      ./sys/i18n.nix
-      ./sys/keyboard.nix
-      ./sys/network.nix
-      ./sys/user.nix
-      ./sys/virtualisation.nix
+      ./modules
       ];
 
-
-
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  # networking.useDHCP = false;
-  # networking.interfaces.enp42s0.useDHCP = true;
-  # networking.interfaces.wlo1.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.dgqyushen = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
@@ -65,7 +19,6 @@
     neofetch
 #    libscret
     man
-    nodejs
     gcc
     gdb
     clang
@@ -73,32 +26,10 @@
     cmake
     ninja
     appimage-run
-    qemu
-    qemu_kvm
-    virt-manager
     iproute
     git
 #    tmux
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
